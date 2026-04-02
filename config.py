42bafafc234078bffa843b7be3cb4ed393dd2432
GattyTang <tangnanking@gmail.com>
2026-04-02 20:34:20 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+KEYWORDS = [
+    "glass beads importer",
+    "reflective material distributor",
+    "road marking contractor company",
+    "traffic safety company",
+    "road marking paint supplier",
+    "thermoplastic road marking company"
+]
+
+# 是否自动发邮件
+AUTO_SEND_EMAILS = False
+
+# 发件邮箱配置
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+SENDER_EMAIL = "your_email@gmail.com"
+SENDER_PASSWORD = "your_app_password"
+
+# 开发信主题
+EMAIL_SUBJECT = "Supply of Glass Beads / Road Marking Materials"
