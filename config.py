4acb38b757498eab16715a0a1ca0b5aed4d1aa8d
GattyTang <tangnanking@gmail.com>
2026-04-02 20:45:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ KEYWORDS = [
 ]
 
 # 是否自动发邮件
-AUTO_SEND_EMAILS = False
+AUTO_SEND_EMAILS = True
 
 # 发件邮箱配置
-SMTP_SERVER = "smtp.gmail.com"
-SMTP_PORT = 587
-SENDER_EMAIL = "your_email@gmail.com"
-SENDER_PASSWORD = "your_app_password"
+SMTP_SERVER = "smtp.126.com"
+SMTP_PORT = 465
+SENDER_EMAIL = "gatty.tang@wanhuitong-logistics.com"
+SENDER_PASSWORD = "B83CDf8f5e4BD8C8"
 
 # 开发信主题
 EMAIL_SUBJECT = "Supply of Glass Beads / Road Marking Materials"
